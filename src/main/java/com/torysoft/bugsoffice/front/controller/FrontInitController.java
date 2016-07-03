52e4d3ac2cc8759271045ca45f8346e96580e83b
david.sun <david.sun@torysoft.net>
2016-07-03 22:27:57 +0900

    move front controller to under front package

--- src/main/java/com/torysoft/bugsoffice/front/controller/FrontInitController.java
+++ src/main/java/com/torysoft/bugsoffice/front/controller/FrontInitController.java
@@ -1,4 +1,4 @@
-package com.torysoft.bugsoffice.front.init;
+package com.torysoft.bugsoffice.front.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
